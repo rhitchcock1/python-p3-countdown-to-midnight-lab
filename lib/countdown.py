fabd78be5534a93bee387ee20392d779b07aab34
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-03-16 17:02:50 -0400

    complete

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,23 @@
 # your code goes here!
+import time
+
+def countdown( x = 10):
+   
+    while x > 0:
+        print(f'{x} SECOND(S)!')
+        x -= 1                   
+    return print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(x = 10):
+  
+    time.sleep(5) 
+    while x > 0:
+        print(f'{x} SECOND(S)!')
+        x -= 1  
+                       
+    return print("HAPPY NEW YEAR!")
+
+
+
+
+  
